a34d718c447252c637b25a3c9bb7f4e78a1d31dd
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-11-11 14:49:29 +0100

    [FIX] Use yolo7 approach for media loader

--- Code/Loader/MediaLoader.py
+++ Code/Loader/MediaLoader.py
@@ -1,29 +1,24 @@
 import cv2
-import numpy as np
 from pathlib import Path
 from yaml import safe_load
-from Code.Classes.Image import Image
+from pymediainfo import MediaInfo
 
 
 class MediaLoader:
 
-    """
-    Load media files from disk or stream from webcam
-    """
+    """Class for loading media (images and videos)"""
 
+    # Constructor
     def __init__(self, path: str) -> None:
 
         """
-            Initialise MediaLoader class starting from a path (string).
+            Initialise ImageVideoLoader class starting from a path (string).
             The path could be either:
             - a directory
             - a file
-            - the string "webcam"
 
             If a directory, all files therein are load recursively.
             If a single file, only that file is processed.
-            If "webcam", the device webcam is turned on and its recording is
-            used as input
 
             Only files with extensions stored in "Settings/format.yaml" are retained.
 
@@ -37,69 +32,164 @@ class MediaLoader:
 
         # Load admissible media formats (either videos or images)
         with open("Settings/format.yaml", "r") as file:
-            formats = safe_load(file)
-            self.formats = formats["image_formats"] + formats["video_formats"]
 
-        # Class initialisation
+            # Load yaml file
+            d = safe_load(file)
 
-        # Case of stream for device webcam
-        if path == "webcam":
-            self.path = None
-            self.mode = "Stream"
-            self.stream = cv2.VideoCapture(0)
+            # Split formats
+            image_formats = d["image_formats"]
+            video_formats = d["video_formats"]
 
-        # Case of actual media (image, video)
+        # Store path and loader mode
+        self.path = Path(path).absolute()
+        self.mode = "Media"
+
+        # Load files
+
+        # If path is a directory, load all files recursively
+        if self.path.is_dir():
+            files = self.path.rglob("*")
+
+        # If is a single file, load it alone
+        elif self.path.is_file():
+            files = [self.path]
+
+        # Else, there has to be an issue
         else:
-            self.path = Path(path).absolute()
-            self.mode = "Media"
+            raise Exception(f"ERROR: {path} does not exist")
 
-            # Load files
+        # Divide between videos and images
+        images = [path for path in files if path.suffix in image_formats]
+        videos = [path for path in files if path.suffix in video_formats]
 
-            # If path is a directory, load all files recursively
-            if self.path.is_dir():
-                files = self.path.rglob("*")
+        # Unite images and videos
+        files = images + videos
 
-            # If is a single file, load it alone
-            elif self.path.is_file():
-                files = [self.path]
+        # Retrieve shapes
+        shapes = list(map(self.get_media_shape, files))
 
-            # Else, there has to be an issue
-            else:
-                raise Exception(f"ERROR: {path} does not exist")
+        # Store info
+        self.files = files
+        self.shapes = shapes
+        self.n_files = len(files)
 
-            # Make sure only admissibile formats are retained
-            files = [file for file in files if file.suffix in self.formats]
+        # If no files are found, raise error
+        assert self.n_files > 0, f"No images or videos found in {path}."
 
-            # Raise error if no admissibile files are left
-            if len(files) == 0:
-                raise Exception(f"ERROR: no admissible files")
+        # Store flag indicating if a file is a video or an image
+        self.video_flag = [False] * len(images) + [True] * len(videos)
 
-            # Assign files to class
-            self.files = [Image.from_path(file.__str__()) for file in files]
+        # If any video is present
+        if any(videos):
 
-    def __getitem__(self, i: int) -> tuple[np.array, str]:
-
-        """Return the i-th element of the Loader, in the form (image, name)
-
-        Input:
-            int: Integer for index of file in the data loader
-
-        Raises:
-            Exception: Raise error when there is an issue with the stream from the webcam
-
-        Returns:
-            tuple: Tuple of NumPy array (the image/frame) and its name, together with suffix.
-        """
-
-        if self.mode == "Stream":
-
-            try:
-                # Capture the stream frame
-                successful, frame = self.stream.read()
-            except:
-                raise Exception(f"ERROR loading the stream")
-
-            return frame, "Stream"
-
+            # Initialise the first one, to be used when iterating
+            self.new_video(videos[0])
         else:
-            return self.files[i].tensor, self.files[i].name
+
+            # If only images are present, set to None
+            self.capture = None
+
+    def __len__(self) -> int:
+
+        """Return length of the Loader i.e. number of files"""
+
+        return self.n_files
+
+    def __iter__(self):
+
+        """Set iteration number when looping"""
+
+        self.count = 0
+
+        return self
+
+    def __next__(self):
+
+        """Iterate through media"""
+
+        # If we reach end of list, stop iteration
+        if self.count == self.n_files:
+            raise StopIteration
+
+        # Select the path to media at hand
+        path = self.files[self.count]
+
+        # If the media is a video
+        if self.video_flag[self.count]:
+
+            # Read framemand store if operation is successful
+            successful, image_BGR = self.capture.read()
+
+            # If not successful, then video has ended
+            if not successful:
+
+                # Increase counter of media files upon completion
+                self.count += 1
+
+                # Release capture as we have completed the video
+                self.capture.release()
+
+                # If all videos have been processed
+                if self.count == self.n_files:
+                    raise StopIteration
+
+                # If it is not the last video, start reading the next one
+                else:
+                    # Retrieve new path after incrementing count
+                    path = self.files[self.count]
+
+                    # Initialise new video
+                    self.new_video(path)
+
+                    # Read framem and store if operation is successful
+                    successful, image_BGR = self.capture.read()
+
+            # Increase frame number by one after reading
+            self.frame += 1
+
+        # If we have an image
+        else:
+            # Read BGR image
+            image_BGR = cv2.imread(path, -1)
+
+            # Increase counter
+            self.count += 1
+
+            # Check that the image exists
+            assert image_BGR is not None, f"Can't read the image at {path}"
+
+        # Convert to RGB
+        image_RGB = image_BGR[:, :, ::-1]
+
+        return path, image_RGB, image_BGR, self.capture
+
+    def get_media_shape(self, media_path: Path) -> tuple:
+
+        """Return (height, width) of media, either
+        image or video"""
+
+        # Parse the media
+        media = MediaInfo.parse(media_path)
+
+        # Only keep video or images
+        track = [
+            track for track in media.tracks if track.track_type in ["Video", "Image"]
+        ][0]
+
+        # Retrieve shape
+        shape = (track.height, track.width)
+
+        return shape
+
+    def new_video(self, path) -> None:
+
+        """Method to be called when a new video is processed"""
+
+        # Initialise frame number
+        self.frame = 0
+
+        # Capture video using openCV
+        self.capture = cv2.VideoCapture(path)
+
+        # Get total number of frames for video at hand
+        self.n_frames = int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
