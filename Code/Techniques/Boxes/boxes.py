dd420b1874f1f8f7e90370c63305ea2f63898d41
giacomopiccinini <piccinini.giacomo@gmail.com>
2022-11-21 19:44:29 +0100

    [CLEANUP]

--- Code/Techniques/Boxes/boxes.py
+++ Code/Techniques/Boxes/boxes.py
@@ -4,36 +4,6 @@ import face_recognition
 from copy import deepcopy
 
 
-def convert_and_trim_bb(image, rectangle):
-
-    """
-    Convert the bounding box expression from
-        (left, right, top, bottom) to
-        (startX, startY, width, height)
-
-    Returns:
-        Tuple: (startX, startY, width, height)
-    """
-    # Extract the dlib bounding box features
-    startX = rectangle.left()
-    startY = rectangle.top()
-    endX = rectangle.right()
-    endY = rectangle.bottom()
-
-    # Ensure the bounding box coordinates fall within the spatial
-    # dimensions of the image
-    startX = max(0, startX)
-    startY = max(0, startY)
-    endX = min(endX, image.shape[1])
-    endY = min(endY, image.shape[0])
-
-    # Compute the width and height of the bounding box
-    width = endX - startX
-    height = endY - startY
-
-    return (startX, startY, width, height)
-
-
 def write_boxes(image, boxes, encodings):
 
     """Write boxes from detection to image"""
@@ -78,9 +48,6 @@ def write_boxes(image, boxes, encodings):
 	
             # Update the list of names
             names.append(name)
-            
-    # Fetch boxes
-    #boxes = [convert_and_trim_bb(image, r.rect) for r in results]
 
     # Create copy of image where to write the boxes
     boxed_image = deepcopy(image)
@@ -95,14 +62,5 @@ def write_boxes(image, boxes, encodings):
      
         # Write text
         cv2.putText(boxed_image, name, (left, text_position), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
-    
-    #cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
-	#y = top - 15 if top - 15 > 15 else top + 15
-	#cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
-	#	0.75, (0, 255, 0), 2)
-
-    # Write bounding box on image
-    #for (x, y, w, h) in boxes:
-    #     cv2.rectangle(boxed_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     return boxed_image
